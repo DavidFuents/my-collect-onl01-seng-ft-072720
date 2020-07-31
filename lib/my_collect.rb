0444ef9f0fbda593e4b1ae3056573ce73918af1f
David Fuentes <davidf0827@gmail.com>
2020-07-31 02:32:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+require "pry"
+def my_collect(array)
+  i = 0 
+  new_collection = []
+  
+  while i < array.length
+    new_collection << yield(array[i])
+    i =+ 1
+    binding.pry
+  end
+  new_collection
+end
 
 
